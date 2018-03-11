638ed83058f4d2f4a1bd0d09b1d58b7d6c3a44da
Justin Degonda <c-justin.degonda@charter.com>
2018-03-11 12:56:20 -0600

    fix: clean code

--- src/index.js
+++ src/index.js
@@ -214,10 +214,10 @@ function getDescriptionListItems(word) {
 
 function getDescriptionResponse(descriptions, word) {
   if (!descriptions.length)
-    return "I did not find any keyboard shortcuts for the keyword" + word;
+    return "I did not find any descriptions that contain " + word;
   if (descriptions.length === 1)
     return (
-      "The only description that matches your keyword" +
+      "The only description that matches your keyword " +
       word +
       "is " +
       descriptions
